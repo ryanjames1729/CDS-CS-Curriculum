10c98e0af1aa7f5ce542abac0084432b2cec379d
Ryan James <ryanjames1729@gmail.com>
2024-09-06 12:42:54 -0400

    lesson 4 updates

---
+++ 8th Grade/Lesson 4/scipt.js
@@ -0,0 +1,15 @@
+let myName = "John Doe";
+let favoriteBook = "To Kill a Mockingbird";
+let favoriteMovie = "The Shawshank Redemption";
+let hometown = "New York City";
+let interestingFact = "I can play the guitar.";
+
+let sentence1 = "My name is " + myName + ".";
+let sentence2 = "I love watching " + favoriteMovie + ".";
+let sentence3 = "An interesting fact about me is that " + interestingFact + ".";
+let sentence4 = "I recommend reading " + favoriteBook + ".";
+let sentence5 = "I'm proud to be from " + hometown + ".";
+
+const paragraph = `${sentence1} ${sentence2} ${sentence3} ${sentence4} ${sentence5}`;
+
+console.log(paragraph);
