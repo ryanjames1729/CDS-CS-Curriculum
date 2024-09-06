fd81644f51de798eaff5e197ea0e3bf1bacad96b
Ryan James <ryanjames1729@gmail.com>
2024-09-06 15:18:36 -0400

    updated lesson 4.2

---
+++ 8th Grade/Lesson 4/Lesson 4.2/script.js
@@ -0,0 +1,5 @@
+const storyParagraph = "Once upon a time, there was a brave knight named Sir Lancelot. He embarked on a perilous quest to rescue a captured princess from the clutches of an evil dragon. Along his journey, he encountered treacherous forests, daunting mountains, and dark caves. With his unwavering courage and sharp sword, Sir Lancelot overcame every obstacle and finally reached the dragon's lair. After a fierce battle, he defeated the dragon and saved the princess, bringing peace and happiness to the kingdom.";
+
+console.log(storyParagraph);
+
+// Type your code below.
